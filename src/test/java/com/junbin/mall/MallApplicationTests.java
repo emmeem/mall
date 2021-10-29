6fd636e8fef659b126508eac3f0c2460c649f436
Junbin Liao <junbin.liao@thoughtworks.com>
2021-10-29 11:14:10 +0800

    [ljb] fix MallApplicationTests

--- src/test/java/com/junbin/mall/MallApplicationTests.java
+++ src/test/java/com/junbin/mall/MallApplicationTests.java
@@ -1,13 +1,9 @@
 package com.junbin.mall;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MallApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
